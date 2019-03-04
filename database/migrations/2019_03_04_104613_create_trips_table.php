536d7bb6bfc2a8cab2ccfa4b97b4e53ffd72c88f
windtagane <windtagane@gmail.com>
2019-03-04 15:14:55 +0400

    Added Auth

---
+++ database/migrations/2019_03_04_104613_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('favorite_place');
+            $table->time('travel_time');
+            $table->integer('average_price');
+            $table->string('transportation');
+            $table->boolean('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
